dd711ec706541714011fd8a88dfb84541cf81f80
kmotohas <kazuki.motohashi@icloud.com>
2016-08-04 19:05:46 +0900

    [add] BasicConfig.py

---
+++ BasicConfig.py
@@ -0,0 +1,5 @@
+import os
+
+username = os.environ.get('USER')
+workdir = '/Users/' + username + '/work/DisplacedVertices/Run2/'
+rootcoredir = '/Users/' + username + '/ATLAS/sw/projects/DV_xAODAnalysis/'
