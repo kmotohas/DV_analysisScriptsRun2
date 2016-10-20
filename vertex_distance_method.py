61ff6870cb07d4e7da749b03aee01192c0538c63
Kazuki Motohashi <kazuki.motohashi@cern.ch>
2016-10-20 22:25:29 +0900

    [update] modify to use another file to scan different events

--- vertex_distance_method.py
+++ vertex_distance_method.py
@@ -13,9 +13,9 @@ input_file_name = args.inputFile
 
 if __name__ == '__main__':
     tfile1 = utils.open_tfile(input_file_name)
-    #tfile2 = utils.open_tfile(input_file_name)
+    tfile2 = utils.open_tfile(input_file_name)
     tree1 = tfile1.Get('Nominal')
-    #tree2 = tfile2.Get('Nominal')
+    tree2 = tfile2.Get('Nominal')
     entries = tree1.GetEntries()
     print('Number of Entries = {}'.format(entries))
 
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     #h_model_z_33 = TH1F('model_z_33', ';Vertex Pair Distance [mm]; Number of Vertices / mm', 250, 0, 250)
     #h_model_3d_33 = TH1F('model_3d_33', ';Vertex Pair Distance [mm]; Number of Vertices / mm', 250, 0, 250)
 
-    for entry in range(entries):
+    for entry in xrange(entries):
         if entry % 10000 == 0:
             print('*****************************')
             print('***** processed ' + str(entry) + ' events')
@@ -54,28 +54,38 @@ if __name__ == '__main__':
         if nb <= 0:
             continue
         for idv, nTracks in enumerate(tree1.DV_nTracks):
+            if len(tree1.DV_nTracks) != len(tree1.DV_r):
+                print('##########################################')
+                print("len(tree1.DV_nTracks) and len(tree1.DV_r) are different for some reason!!!")
+                print(entry, len(tree1.DV_nTracks), len(tree1.DV_r))
+                continue
             rIndex = utils.get_region(tree1, idv)
             # interested in only vertices of nTracks == 2 or 3
             if nTracks > 3 or not rIndex == 0:
                 continue
             vec1 = TVector3(tree1.DV_x[idv], tree1.DV_y[idv], tree1.DV_z[idv])
             # model construction
-            for entry2 in range(entry+1, entries):
+            for entry2 in xrange(entry+1, entries):
                 utils.show_progress(entry2, entries-entry)
                 # get the next tree in the chain and verify
-                ientry2 = tree1.LoadTree(entry2)
+                ientry2 = tree2.LoadTree(entry2)
                 if ientry2 < 0:
                     break
                 # copy next entry into memory and verify
-                nb2 = tree1.GetEntry(entry2)
+                nb2 = tree2.GetEntry(entry2)
                 if nb2 <= 0:
                     continue
-                for idv2, nTracks2 in enumerate(tree1.DV_nTracks):
-                    rIndex = utils.get_region(tree1, idv2)
+                for idv2, nTracks2 in enumerate(tree2.DV_nTracks):
+                    if len(tree2.DV_nTracks) != len(tree2.DV_r):
+                        print('##########################################')
+                        print("len(tree2.DV_nTracks) and len(tree2.DV_r) are different for some reason!!!")
+                        print(entry2, len(tree2.DV_nTracks), len(tree2.DV_r))
+                        continue
+                    rIndex = utils.get_region(tree2, idv2)
                     # interested in only vertices of nTracks == 2 or 3
                     if nTracks2 > 3 or not rIndex == 0:
                         continue
-                    vec2 = TVector3(tree1.DV_x[idv2], tree1.DV_y[idv2], tree1.DV_z[idv2])
+                    vec2 = TVector3(tree2.DV_x[idv2], tree2.DV_y[idv2], tree2.DV_z[idv2])
                     dist_3d = (vec1-vec2).Mag()
                     dist_xy = (vec1-vec2).Perp()
                     dist_z = TMath.Abs((vec1-vec2).Z())
